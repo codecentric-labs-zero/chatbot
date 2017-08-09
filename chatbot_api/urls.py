ec894df3866a490c86729a23c369fc71fa6b8394
milansusnjar-cc <milan.susnjar@codecentric.de>
2017-08-09 11:03:16 +0200

    Add url for receiving sms

--- chatbot_api/urls.py
+++ chatbot_api/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from chatbot_api import views
 
 urlpatterns = [
-    url(r'^hello_world$', views.hello_world)
+    url(r'^hello_world$', views.hello_world),
+    url(r'^receive_sms$', views.receive_sms),
     ]
